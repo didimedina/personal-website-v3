c94b1e7763df876fec57dc61281825295cc56172
Didi Medina <didi@Didis-MacBook-Pro.local>
2022-02-27 08:54:54 +0200

    added framer motion and got stitches up and running without theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,16 @@
 // import type { NextPage } from 'next'
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'; 
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
-function autoDark(color){
 
-  let modifiedColor = `dark:bg-dark-${color}`
-
-  return modifiedColor
-}
-
-const isDarkMode = true
 
 export default function Home() {
   return (
-    <div className={`w-auto h-screen flex justify-center items-center bg-slate3 dark:bg-dark-slate3`}>
+    <div className={`w-auto h-screen flex justify-center items-center bg-slate-4 outline-none`}>
       <DropdownMenu.Root>
-        <DropdownMenu.Trigger className="h-8">Settings</DropdownMenu.Trigger>
+        <DropdownMenu.Trigger className="h-8 bg-slate-6 px-4 rounded-md focus-visible:outline-none">Settings</DropdownMenu.Trigger>
 
-        <DropdownMenu.Content>
-          <DropdownMenu.Item>Item One</DropdownMenu.Item>
+        <DropdownMenu.Content className="w-44 overflow-hidden border border-slate-8 border-solid rounded-md mt-1 ">
+          <DropdownMenu.Item className="border-b border-slate-8 px-2 py-1 hover:bg-slate-6 hover:outline-none">Item One</DropdownMenu.Item>
           <DropdownMenu.Item>Item Two</DropdownMenu.Item>
           <DropdownMenu.Item>Item Three</DropdownMenu.Item>
           <DropdownMenu.CheckboxItem>Click Me</DropdownMenu.CheckboxItem>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const radixColors = require('@radix-ui/colors')
 
-console.log(radixColors.slateDark)
-console.log(radixColors.slate)
+// console.log(radixColors.slateDark)
+// console.log(radixColors.slate)
 
 module.exports = {
   darkMode: 'class',
@@ -13,15 +13,38 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      
-      
-      ...radixColors.slate,
-      
-      
-      dark: {
-        // 'slate1': mode? slate1 : slateDark1
-        ...radixColors.slateDark,
-      }
+      slate: {
+        '1': 'var(--slate1)',
+        '2': 'var(--slate2)',
+        '3': 'var(--slate3)',
+        '4': 'var(--slate4)',
+        '5': 'var(--slate5)',
+        '6': 'var(--slate6)',
+        '7': 'var(--slate7)',
+        '8': 'var(--slate8)',
+        '9': 'var(--slate9)',
+        '10': 'var(--slate10)',
+        '11': 'var(--slate11)',
+        '12': 'var(--slate12)',
+        // dark: {
+        //   '1': rgb(21, 23, 24);
+        //   '2': rgb(26, 29, 30);
+        //   '3': rgb(32, 36, 37);
+        //   '4': rgb(38, 41, 43);
+        //   '5': rgb(43, 47, 49);
+        //   '6': rgb(49, 53, 56);
+        //   '7': rgb(58, 63, 66);
+        //   '8': rgb(76, 81, 85);
+        //   '9': rgb(105, 113, 119);
+        //   '10': rgb(120, 127, 133);
+        //   '11': rgb(155, 161, 166);
+        //   '12': rgb(236, 237, 238);
+        // }
+      },      
+      // dark: {
+      //   // 'slate1': mode? slate1 : slateDark1
+      //   ...radixColors.slateDark,
+      // }
       // how do you get this to be implicit at build time?
 
     },
